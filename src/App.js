f73dc06bb2298fdf678bd66bb8a67ae7bc408990
Funbii oye <funbiioye@gmail.com>
2021-06-28 13:29:58 +0100

    complete todo app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList.js'
+
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
